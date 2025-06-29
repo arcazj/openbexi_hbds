7ef8668ba64318880a12ffcca8587733778cb147
arcazj <arcazj@gmail.com>
2025-06-29 19:37:33 -0400

    openbexi_hbds V1.0 - improve draggability.

--- hbds_class.js
+++ hbds_class.js
@@ -2,7 +2,6 @@
 import * as THREE from 'three';
 import { CSS2DObject } from 'three/addons/renderers/CSS2DRenderer.js';
 
-
 /* ─────────────────────────────── Loader ──────────────────────────────── */
 export const Loader = {
     fallbackHumanModel: {
